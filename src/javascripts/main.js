31781e11f7aeb877013dae97bd786fee4d2fe6d7
Mary West <westmarye1219@gmail.com>
2019-04-20 11:24:36 -0500

    locations data printing to the page

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,9 +1,11 @@
 import '../styles/main.scss';
 import 'bootstrap';
 import movies from './components/movies/movies';
+import locations from './components/locations/locations';
 
 const init = () => {
   movies.initializeMovies();
+  locations.initializeLocations();
 };
 
 init();
